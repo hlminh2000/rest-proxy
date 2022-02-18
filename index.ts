edbadbc0d585e3a99d40dab4086e848c779af3d1
Minh <hlminh2000@gmail.com>
2022-02-18 13:59:58 -0500

    use fetch

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { response } from "express";
 import morgan from "morgan";
 import { createProxyMiddleware } from "http-proxy-middleware";
 import bodyParser from "body-parser";
+import fetch from "isomorphic-fetch";
 
 const PORT = process.env.PORT || 5000;
 const TARGET_HOST = process.env.TARGET_HOST || "https://google.com";
@@ -11,12 +12,23 @@ const app = express();
 app.use(
   "*",
   bodyParser.json(),
-  morgan("combined"),
+  //   morgan("combined"),
   (req, res, next) => {
     console.log("body: ", req.body);
     next();
   },
-  createProxyMiddleware({ target: TARGET_HOST, changeOrigin: true })
+  //   createProxyMiddleware({ target: TARGET_HOST, changeOrigin: true }),
+  async (req, res) => {
+    const respose = await fetch(
+      `${TARGET_HOST}/dev/jumioProcessCddApplication`,
+      {
+        method: "POST",
+        body: JSON.stringify(req.body),
+      }
+    ).then((res: any) => res.json());
+    console.log("response: ", response);
+    res.send(respose);
+  }
 );
 
 app.listen(PORT, () => {
